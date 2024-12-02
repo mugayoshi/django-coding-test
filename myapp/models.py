1dfb49bee80632407bb77b045c5e6effcb8b229e
Muga Yoshikawa <muga.yoshikawa@chartboost.com>
2024-12-02 14:52:12 +0100

    feat: add mypy check

--- myapp/models.py
+++ myapp/models.py
@@ -3,9 +3,9 @@ from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Content(models.Model):
-    title = models.CharField(max_length=255)
-    metadata = models.JSONField(default=dict)
-    rating = models.DecimalField(
+    title: models.CharField = models.CharField(max_length=255)
+    metadata: models.JSONField = models.JSONField(default=dict)
+    rating: models.DecimalField = models.DecimalField(
         max_digits=4, 
         decimal_places=2, 
         validators=[MinValueValidator(0), MaxValueValidator(10)]
@@ -16,21 +16,21 @@ class Content(models.Model):
 
 
 class ContentFile(models.Model):
-    content = models.ForeignKey(Content, on_delete=models.CASCADE, related_name='files')
-    file = models.FileField(upload_to='content_files/')
+    content: models.ForeignKey = models.ForeignKey(Content, on_delete=models.CASCADE, related_name='files')
+    file: models.FileField = models.FileField(upload_to='content_files/')
 
     def __str__(self):
         return f"File for {self.content.title} - {self.file.name}"
 
 
 class Channel(models.Model):
-    title = models.CharField(max_length=255)
-    language = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='channel_pictures/', blank=True, null=True)
-    parent_channel = models.ForeignKey(
+    title: models.CharField = models.CharField(max_length=255)
+    language: models.CharField = models.CharField(max_length=100)
+    picture: models.ImageField = models.ImageField(upload_to='channel_pictures/', blank=True, null=True)
+    parent_channel: models.ForeignKey = models.ForeignKey(
         'self', on_delete=models.CASCADE, related_name='subchannels', blank=True, null=True
     )
-    contents = models.ManyToManyField(Content, blank=True)
+    contents: models.ManyToManyField = models.ManyToManyField(Content, blank=True)
 
     def clean(self):
         if self.pk is None:
